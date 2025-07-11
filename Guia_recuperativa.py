c22e1cefdaaec0a1453042ce2f06097d62a28b74
Felipe <felipemendez343@gmail.com.>
2025-07-10 20:00:04 -0400

    Agregacion de muestra y Analisis

--- Guia_recuperativa.py
+++ Guia_recuperativa.py
@@ -16,7 +16,37 @@ class Gen():
     def set_expresion(self,Nueva_expresion):
         if isinstance(Nueva_expresion,str):
             self.__expresion=Nueva_expresion
-    def get_nombre(self):
+    def get_expresion(self):
         return self.__expresion
+
+    def obtener_info(self):
+        return self.__nombre,self.__longitud,self.__expresion
+
+    def print_Gen(self):
+        print(f"Nombre: {self.__nombre}. \n longitud: {self.__longitud}. \n Expresion: {self.__expresion}.")    
     
+class Analisis():
+    def __init__(self):
+        self.__resultados=[]
+    def agregar_resultado(self,Nuevo_resultado):
+            resultados.append(Nuevo_resultado)
+
+class Muestra():
+    def __init__(self):
+        self.__nombre=None
+        self.__Genes=[]
+    def agregar_Gen(self,Nuevo_gen):
+        if isinstance(Nuevo_gen,Gen):
+            if Nuevo_gen in self.__Genes:
+                return
+            else:
+                self.__Genes.append(Nuevo_gen)
+    def get_nombre(self):
+        return self.__nombre
+    def set_nombre(self,Nuevo_nombre):
+        if isinstance(Nuevo_nombre,str):
+            self.__nombre=Nuevo_nombre
+        else:
+            print("Error a ingresar nuevo nombre a la muestra")    
+
 
